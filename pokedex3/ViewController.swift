c91162e2b5d9d2c42dd8fbfc8e30f5aa07c6bcb5
Chatnattaphon Ratnaratorn <i_first@hotmail.com>
2019-02-09 01:23:31 +0700

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Chatnattaphon Ratnaratorn on 9/2/2562 BE.
+//  Copyright © 2562 Chatnattaphon Ratnaratorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
